3595ae44963e1e4a1c2136a73f31b5678c5edcf9
SVGAnimate <76270254+SVGAnimate@users.noreply.github.com>
2021-03-08 13:43:05 +0100

    Update use_g_list_append.c

--- use_g_list_append.c
+++ use_g_list_append.c
@@ -7,12 +7,20 @@ main(int argc, char *argv[]) {
     gchar *key3 = "key3";
 
     typedef GList GList_CStr;
+    GList_CStr *list;
     
-    GList_CStr *list = NULL;
-    list = g_list_append(list, key1);
+    list = g_list_append(NULL, key1);
     g_list_append(list, key2);
     g_list_append(list, key3);
     
+    // output list :
+    // "key1"
+    // "key2"
+    // "key3"
+    
+    list = g_list_prepend(NULL, key1);//            output list : {0: key1}
+    g_list_prepend(list, key2);//          output list : {0: key2, 1: key1}
+    g_list_prepend(list, key3);// output list : {0: key2, 1: key3, 2: key1}
     // output list :
     // "key2"
     // "key3"
