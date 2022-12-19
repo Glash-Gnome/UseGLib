75ba76d6d2b0b00a0d277fc5342c98bf0950f752
SVGAnimate <glash.gnome@gmail.com>
2022-12-19 20:00:43 +0100

    Update use_g_type_register_dynamic.c

--- use_g_type_register_dynamic.c
+++ use_g_type_register_dynamic.c
@@ -1,10 +1,13 @@
+/**
+ * all: use_g_type_register_dynamic.c
+ *	gcc use_g_type_register_dynamic.c -o main `pkg-config --cflags --libs gobject-2.0`
+ */
 #include <gmodule.h>
 
 int
 main (int argc, char *argv[])
 {
-    const char *c_str = "My-Object";
-
+    char *c_str = "My-Object";
 
     return 0;
 }
